d699fcf630572c52b0167785df1115168a024e00
l saikumar <l saikumar@LAPTOP-QBLG0IJM>
2022-06-20 17:49:16 +0530

    hi

--- src/abc/InstaVar.java
+++ src/abc/InstaVar.java
@@ -7,7 +7,7 @@ public class InstaVar {
 	public static void main(String[] args) {
 	InstaVar abc=new InstaVar() ;
 	int finaloutput=abc.total(5,526);
-	System.out.println(finaloutput);
+	System.out.println("finaloutput");
 		}
 		 int total(int days,int price){
 			int result=(days*price);
